0f1756a0fc6fd0e7125f84319c1ed108abbf657b
AL512 <AL512@mail.ru>
2023-02-28 19:43:02 +0700

    feat(frontend): Добавление api функции обновления (update)

--- cardealership.frontend/src/api/ClientBase.ts
+++ cardealership.frontend/src/api/ClientBase.ts
@@ -4,8 +4,8 @@ import {
     ICarLookupDto,
     ICreateCarDto,
     IProblemDetails,
-    ICarDetails
-    }
+    ICarDetails, IUpdateCarDto
+}
     from '../Interfases/CarInterfases'
 
 export enum ApiObject {
@@ -374,6 +374,72 @@ export class ClientBase {
         return Promise.resolve<TDetails>(<any>null);
     }
 
+    update<TUpdate>(version: string, apiObject: ApiObject, body: TUpdate, ): Promise<void> {
+        let url_ = this.baseUrl + '/api/{version}/{apiObject}';
+        if (version === undefined || version === null)
+            throw new Error("The parameter 'version' must be defined.");
+        url_ = url_.replace('{version}', encodeURIComponent('' + version));
+        url_ = url_.replace('{apiObject}', encodeURIComponent('' + apiObject));
+        url_ = url_.replace(/[?&]$/, '');
+
+        const content_ = JSON.stringify(body);
+
+        let options_ = <RequestInit>{
+            body: content_,
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        };
+
+        return this.transformOptions(options_)
+            .then((transformedOptions_) => {
+                return this.http.fetch(url_, transformedOptions_);
+            })
+            .then((_response: Response) => {
+                return this.processUpdate(_response);
+            });
+    }
+
+    protected processUpdate(response: Response): Promise<void> {
+        const status = response.status;
+        let _headers: any = {};
+        if (response.headers && response.headers.forEach) {
+            response.headers.forEach((v: any, k: any) => (_headers[k] = v));
+        }
+        if (status === 204) {
+            return response.text().then((_responseText) => {
+                return;
+            });
+        } else if (status === 401) {
+            return response.text().then((_responseText) => {
+                let result401: any = null;
+                result401 =
+                    _responseText === ''
+                        ? null
+                        : <IProblemDetails>(
+                            JSON.parse(_responseText, this.jsonParseReviver)
+                        );
+                return this.throwException(
+                    'Unauthorized',
+                    status,
+                    _responseText,
+                    _headers,
+                    result401
+                );
+            });
+        } else if (status !== 200 && status !== 204) {
+            return response.text().then((_responseText) => {
+                return this.throwException(
+                    'An unexpected server error occurred.',
+                    status,
+                    _responseText,
+                    _headers
+                );
+            });
+        }
+        return Promise.resolve<void>(<any>null);
+    }
 
 
     protected isApiException = true;
