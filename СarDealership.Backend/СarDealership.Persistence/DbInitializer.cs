9e265929ae87040883aaadc4175dfb88ee822360
AL512 <AL512@mail.ru>
2023-03-05 15:11:08 +0700

    feat(backend): Заполнение БД тестовыми данными

--- СarDealership.Backend/СarDealership.Persistence/DbInitializer.cs
+++ СarDealership.Backend/СarDealership.Persistence/DbInitializer.cs
@@ -1,4 +1,7 @@
-﻿namespace СarDealership.Persistence
+﻿using System;
+using СarDealership.Domain.CarInfo;
+
+namespace СarDealership.Persistence
 {
     /// <summary>
     /// Инициализация БД
@@ -13,6 +16,24 @@
         {
             //context.Database.EnsureDeleted();
             context.Database.EnsureCreated();
+            context.Cars.AddRange(
+                new Car
+                {
+                    Name = "ВАЗ 2109",
+                    UserId = Guid.Parse("98D51994-5CC4-44E2-B720-69FF4915EA42"),
+                    Pow = 98,
+                    Long = 3970,
+                    Price = 45787
+                },
+                new Car
+                {
+                    Name = "Toyota Corolla",
+                    UserId = Guid.Parse("98D51994-5CC4-44E2-B720-69FF4915EA42"),
+                    Pow = 127,
+                    Long = 4098,
+                    Price = 748512
+                });
+            context.SaveChanges();
         }
     }
 }
